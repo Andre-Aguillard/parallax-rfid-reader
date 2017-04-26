258fecb887e4d54c5f01cd29502bda9ee76dbe87
Andre Aguillard <AguillardDre@gmail.com>
2017-04-26 10:45:03 -0500

    Update parallax.py
    Rough update

--- parallax.py
+++ parallax.py
@@ -24,12 +24,13 @@ window = Tk()
 window.title("Better Bike Lock Home")
 
 
-
+#Define variables
 
 ENABLE_PIN  = 18              # The BCM pin number corresponding to GPIO1
 SERIAL_PORT = '/dev/ttyAMA0'  # The location of our serial port.  This may
                               # vary depending on OS version.
-
+  
+NUMBER_OF_LOCKS = 2           #set the number of locks
 
 def validate_rfid(code):
     # A valid code will be 12 characters long with the first char being
@@ -51,7 +52,7 @@ def main():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
 
-    # This pin corresponds to GPIO1, which we'll use to turn the RFID
+    # This pin corresponds to GPIO18, which we'll use to turn the RFID
     # reader on and off with.
     GPIO.setup(ENABLE_PIN, GPIO.OUT)
 
@@ -68,6 +69,38 @@ def main():
                         port     = SERIAL_PORT,
                         stopbits = serial.STOPBITS_ONE,
                         timeout  = 1)
+    
+    # Create a dictionary that associates the bike lock number with a code -Aguillard
+    locks{}
+    # Create a list of empty locks based initially off the number of locks -Aguillard
+    # in the system
+    emptyLocks = [x in range len(1,NUMBER_OF_LOCKS+1)]
+    
+# Assigns a code to a lock
+def assignLock(code):
+    locks[code] = emptyLocks[0]
+    # If we assign a lock, we must then open that lock for the user to use.
+    #openLock(emptyLocks[0])
+    del emptyLocks[0]
+    
+    
+# Checks to see if there are any open locks
+def checkLocks():
+    # If there are no empty locks, then there's nothing else to do.
+    if (emptyLocks = []):
+        response = "Sorry there are no empty locks at this time, \
+                    please try again later."
+    # If there are emptylocks, then assign a lock
+    else:
+        assignLock(code)
+        response = "Assigning you a lock..."
+        
+        
+    
+  
+# Unlocks a lock and adds a that lock back to the list of empty locks
+def unlockLock(code):
+    pass
 
     # Wrap everything in a try block to catch any exceptions.
     try:
@@ -78,10 +111,22 @@ def main():
 
             # Attempt to validate the data we just read.
             code = validate_rfid(data)
-
+            
             # If validate_rfid() returned a code, display it.
             if code:
                 print("Read RFID code: " + code);
+                #check to see if the 
+                if (code in locks) :
+                  unlockLock(code)
+                  # Set the response. 
+                  ## The response will be display on the GUI once that is finished
+                  response = "Thank you for using a better bike lock, \
+                          We appreciate your business. Have a great day!"
+                #If its a new code, check to see if there are any open locks
+                else:
+                  checkLocks(code)
+                  response = "Checking for open locks..."
+                
     except:
         # If we caught an exception, then disable the reader by setting
         # the pin to HIGH, then exit.
@@ -92,3 +137,4 @@ def main():
 if __name__ == "__main__":
     main()
 
+   
